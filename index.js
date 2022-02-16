b76012308c500546d8b8bb20acb15437928e1c5c
Eliyas-Mohamed <61540226+Eliyas-Mohamed@users.noreply.github.com>
2022-02-16 21:37:49 +0530

    audio path added

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ document.querySelectorAll('button').forEach((button)=>button.addEventListener('c
 function playSound(key){
     switch(key){
         case'w':
-            const wAudio=new Audio('/sounds/crash.mp3');
+            const wAudio=new Audio('crash.mp3');
             wAudio.play();
             animation(key);
             break;
         case'a':
-            const aAudio=new Audio('/sounds/kick-bass.mp3');
+            const aAudio=new Audio('kick-bass.mp3');
             aAudio.play();
             animation(key);
             break;
         case's':
-            const sAudio=new Audio('/sounds/snare.mp3');
+            const sAudio=new Audio('snare.mp3');
             sAudio.play();
             animation(key);
             break;
         case'd':
-            const dAudio=new Audio('/sounds/tom-1.mp3');
+            const dAudio=new Audio('tom-1.mp3');
             dAudio.play();
             animation(key);
             break;
         case'j':
-            const jAudio=new Audio('/sounds/tom-2.mp3');
+            const jAudio=new Audio('tom-2.mp3');
             jAudio.play();
             animation(key);
             break;
         case'k':
-            const kAudio=new Audio('/sounds/tom-3.mp3');
+            const kAudio=new Audio('tom-3.mp3');
             kAudio.play();
             animation(key);
             break;
         case'l':
-            const lAudio=new Audio('/sounds/tom-4.mp3');
+            const lAudio=new Audio('tom-4.mp3');
             lAudio.play();
             animation(key);
             break;
